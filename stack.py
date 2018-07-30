c31733f64e1be363c7f3973f0c8a23e03fdad6d6
Ankan Poddar <friendyankan@gmail.com>
2018-07-30 17:58:21 +0530

    Change calc.py to main.py

---
+++ stack.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+
+class Stack:
+    """Implementation of the stack ADT, this will be required for evaluating
+    the expressions of the calculator class.
+    """
+    def __init__(self):
+        self.items = []
+
+    def isEmpty(self):
+        return self.items == []
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        return self.items.pop()
+
+    def peek(self):
+        return self.items[-1]
+
+    def size(self):
+        return len(self.items)
